75f5db471703056c28f510d3a2b642ee6451ea84
lina <mad.20th@gmail.com>
2024-01-08 15:34:14 +0900

    first commit

---
+++ pages/works/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+const { data, pending, error, refresh } = await useFetch('http://localhost:3000/api/works')
+console.log(data)
+</script>
+
+
+<template>
+  <div>
+        {{ data }}
+  </div>
+</template>
