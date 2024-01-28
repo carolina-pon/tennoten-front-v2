8f276e25a1af3e7fe9fb9848c3c96f390f6d5c78
lina <mad.20th@gmail.com>
2024-01-28 20:25:52 +0900

    Add: 作品アーカイブページ作成

---
+++ pages/works/2021/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+const { data: works } = await useFetch("http://localhost:3000/api/works");
+const { data: authors } = await useFetch("http://localhost:3000/api/authors");
+</script>
+
+<template>
+  <h1>点の展2021 at oneline</h1>
+  <div v-for="work in works" :key="work.id">
+    <p>タイトル：{{ work.title }}</p>
+    {{ work.description }}
+  </div>
+</template>
