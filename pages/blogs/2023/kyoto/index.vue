a903dee1bc09015697cd2d675837751643010034
lina <mad.20th@gmail.com>
2024-02-18 15:04:38 +0900

    Add: ブログ仮デザイン作成

---
+++ pages/blogs/2023/kyoto/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <h1 class="text-h5 text-center">INTERVIEW BLOG 2023-KYOTO</h1>
+
+  <v-container>
+    <v-row dense justify="center">
+      <v-col cols="4">
+        <v-card variant="outlined">
+          <div class="d-flex flex-no-wrap justify-space-between">
+            <v-avatar
+              class="ma-3"
+              size="125"
+              rounded="5"
+              color="surface-variant"
+            >
+            </v-avatar>
+            <v-card-title
+              class="text-h5"
+              style="
+                display: flex;
+                justify-content: center;
+                align-items: center;
+              "
+              >永田　里奈</v-card-title
+            >
+          </div>
+        </v-card>
+      </v-col>
+      <v-col cols="4">
+        <v-card variant="outlined">
+          <div class="d-flex flex-no-wrap justify-space-between">
+            <v-avatar
+              class="ma-3"
+              size="125"
+              rounded="5"
+              color="surface-variant"
+            >
+            </v-avatar>
+            <v-card-title
+              class="text-h5"
+              style="
+                display: flex;
+                justify-content: center;
+                align-items: center;
+              "
+              >永田　里奈</v-card-title
+            >
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
