b001aae89094b6cb8a27acae91fddacbae9ecf66
lina <mad.20th@gmail.com>
2024-05-23 22:36:55 +0900

    add: 作品アーカイブ一覧作成

--- pages/works/2021/index.vue
+++ pages/works/2021/index.vue
@@ -1,12 +1,45 @@
-<script setup>
-const { data: works } = await useFetch("http://localhost:3000/api/works");
-const { data: authors } = await useFetch("http://localhost:3000/api/authors");
-</script>
-
 <template>
-  <h1>点の展2021 at oneline</h1>
-  <div v-for="work in works" :key="work.id">
-    <p>タイトル：{{ work.title }}</p>
-    {{ work.description }}
-  </div>
+  <p class="text-h5 text-center">
+    点の展 2021 AT ONELINE <v-icon icon="mdi-web"></v-icon> 
+  </p>
+  <v-container>
+  <v-row>
+    <v-col cols="12" md="4">
+      <v-card class="mx-auto" max-width="350px">
+        <v-img
+          height="200px"
+          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+          cover
+        ></v-img>
+
+        <v-card-title>夜空の星</v-card-title>
+          <v-card-text>永田　里奈</v-card-text>
+      </v-card>
+    </v-col>
+    <v-col cols="12" md="4">
+      <v-card class="mx-auto" max-width="350px">
+        <v-img
+          height="200px"
+          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+          cover
+        ></v-img>
+
+        <v-card-title>夜空の星</v-card-title>
+          <v-card-text>永田　里奈</v-card-text>
+      </v-card>
+    </v-col>
+    <v-col cols="12" md="4">
+      <v-card class="mx-auto" max-width="350px">
+        <v-img
+          height="200px"
+          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+          cover
+        ></v-img>
+
+        <v-card-title>夜空の星</v-card-title>
+          <v-card-text>永田　里奈</v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</v-container>
 </template>
