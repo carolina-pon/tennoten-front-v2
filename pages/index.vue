f125b0e800cf56febf2a8ed4e590b89d33fd3344
lina <mad.20th@gmail.com>
2024-05-23 16:36:04 +0900

    modify: TOPページのデザイン変更

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,27 @@
-<!-- <template>
-  <v-app-bar>
-    <template v-slot:prepend>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-    </template>
-    <v-app-bar-title>点の展</v-app-bar-title>
-  </v-app-bar>
-</template> -->
-
 <template>
+  <v-container>
   <v-layout class="rounded rounded-md">
-    <v-app-bar>
-      <v-app-bar-title>点の展</v-app-bar-title>
+    <v-app-bar flat>
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-title>点の展</v-app-bar-title>
+      <v-btn icon href="https://twitter.com/tennoten_">
+        <v-icon>mdi-twitter</v-icon>
+      </v-btn>
+      <v-btn icon href="https://www.instagram.com/tennoten_/">
+        <v-icon>mdi-instagram</v-icon>
+      </v-btn>
     </v-app-bar>
 
-    <v-navigation-drawer>
-      <v-list>
-        <v-list-item title="Navigation drawer"> </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-main
-      class="d-flex align-center justify-center"
-      style="min-height: 300px"
-    >
-      Main Content
-      <Timeline
-        id="twitterdev"
-        sourceType="profile"
-        :options="{ tweetLimit: '3' }"
-      />
+    <v-main>
+      <v-img src="~/assets/main.png" max-width="100%"> </v-img>
     </v-main>
   </v-layout>
   <v-footer>
     <v-col class="text-center mt-4" cols="12"
-      >2024 <strong>点の展</strong>
+      >©︎2024 <strong>点の展</strong>
     </v-col>
   </v-footer>
+</v-container>
 </template>
+
+<script setup></script>
