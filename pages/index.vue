4227f0b4cb8d8141fc4a2fb23efed853d8089890
lina <mad.20th@gmail.com>
2024-05-23 17:26:59 +0900

    modify: TOPページのデザイン完成

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,30 @@
   <v-container>
   <v-layout class="rounded rounded-md">
     <v-app-bar flat>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-menu
+      transition="slide-y-transition"
+    >
+      <template v-slot:activator="{ props }">
+        <v-btn
+          v-bind="props"
+        ><v-icon>mdi-menu</v-icon>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+
       <v-app-bar-title>点の展</v-app-bar-title>
+      <v-btn>ABOUT</v-btn>
+      <v-btn>MEMBERS</v-btn>
+      <v-btn>INTERVIEW BLOG</v-btn>
+      <v-btn>ARCHIVES</v-btn>
       <v-btn icon href="https://twitter.com/tennoten_">
         <v-icon>mdi-twitter</v-icon>
       </v-btn>
@@ -24,4 +46,11 @@
 </v-container>
 </template>
 
-<script setup></script>
+<script setup>
+  const items=  [
+        { title: 'ABOUT' },
+        { title: 'MEMBERS' },
+        { title: 'INTERVIEW BLOG' },
+        { title: 'ARCHIVES' },
+      ]
+</script>
