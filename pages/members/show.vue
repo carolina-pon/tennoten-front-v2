27187d9c89c4b59d476086fe5b2cbee45beb54ff
lina <mad.20th@gmail.com>
2024-05-23 23:03:26 +0900

    add: 作者詳細ページ作成

---
+++ pages/members/show.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" md="4">
+        <div class="d-flex justify-center">
+          <v-avatar size="350">
+            <v-img src="https://cdn.vuetifyjs.com/images/john.jpg"></v-img>
+          </v-avatar>
+        </div>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="12" md="4">
+        <v-divider color="black"></v-divider>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="12" md="4">
+        <p class="text-h5 d-flex justify-center">
+          <span>名前：</span>永田　里奈
+        </p>
+        <p class="text-h5 d-flex justify-center"><span>活動拠点：</span>東京</p>
+        <p class="text-h5 d-flex justify-center">
+          <span>SNS：</span>Instagram: aaa
+        </p>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="12" md="4">
+        <v-divider color="black"></v-divider>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
