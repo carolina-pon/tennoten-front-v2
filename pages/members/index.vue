040618af1ccc6120cd99a377d8e55c9b09e48f5b
lina <mad.20th@gmail.com>
2024-05-24 13:01:49 +0900

    modify: メンバー一覧を取得

--- pages/members/index.vue
+++ pages/members/index.vue
@@ -1,51 +1,21 @@
+<script setup>
+const { data: authors } = await useFetch("http://localhost:3000/api/authors");
+</script>
+
 <template>
   <p class="text-h5 text-center">MEMBERS</p>
   <v-container>
     <v-row>
-      <v-col cols="12" md="4">
+      <v-col cols="4" md="4" v-for="author in authors" :key="author.id">
         <v-card>
           <v-card-item>
             <div class="d-flex justify-center">
               <v-avatar size="250">
-                <v-img
-                  src="https://cdn.vuetifyjs.com/images/john.jpg"
-                ></v-img>
+                <v-img src="https://cdn.vuetifyjs.com/images/john.jpg"></v-img>
               </v-avatar>
             </div>
             <v-divider :thickness="9" color="white"></v-divider>
-            <p class="text-h5 d-flex justify-center">永田　里奈</p>
-          </v-card-item>
-        </v-card>
-      </v-col>
-
-      <v-col cols="12" md="4">
-        <v-card>
-          <v-card-item>
-            <div class="d-flex justify-center">
-              <v-avatar size="250">
-                <v-img
-                  src="https://cdn.vuetifyjs.com/images/john.jpg"
-                ></v-img>
-              </v-avatar>
-            </div>
-            <v-divider :thickness="9" color="white"></v-divider>
-            <p class="text-h5 d-flex justify-center">永田　里奈</p>
-          </v-card-item>
-        </v-card>
-      </v-col>
-
-      <v-col cols="12" md="4">
-        <v-card>
-          <v-card-item>
-            <div class="d-flex justify-center">
-              <v-avatar size="250">
-                <v-img
-                  src="https://cdn.vuetifyjs.com/images/john.jpg"
-                ></v-img>
-              </v-avatar>
-            </div>
-            <v-divider :thickness="9" color="white"></v-divider>
-            <p class="text-h5 d-flex justify-center">永田　里奈</p>
+            <p class="text-h5 d-flex justify-center">{{ author.name }}</p>
           </v-card-item>
         </v-card>
       </v-col>
