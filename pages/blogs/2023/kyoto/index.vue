608b3e43bd86de15203e6f5bed9593adba164e55
lina <mad.20th@gmail.com>
2024-05-26 19:36:15 +0900

    modify:  ブログ一覧を取得

--- pages/blogs/2023/kyoto/index.vue
+++ pages/blogs/2023/kyoto/index.vue
@@ -1,10 +1,20 @@
+<script setup>
+const { data: authors } = await useFetch("http://localhost:3000/api/authors");
+const { data: blogs } = await useFetch("http://localhost:3000/api/blogs");
+
+const getAuthorName = (authorId) => {
+  const author = authors.value.find((author) => author.id === authorId);
+  return author ? author.name : "";
+};
+</script>
+
 <template>
   <p class="text-h5 text-center">
     INNTERVIEW BLOG 2023<v-icon icon="mdi-fountain-pen"></v-icon> IN KYOTO
   </p>
   <v-container>
     <v-row>
-      <v-col cols="12" md="4">
+      <v-col cols="12" md="4" v-for="blog in blogs" :key="blog.id">
         <v-card>
           <v-card-item>
             <div class="d-flex justify-center">
@@ -13,33 +23,7 @@
               </v-avatar>
             </div>
             <v-divider :thickness="9" color="white"></v-divider>
-            <p class="text-h5 d-flex justify-center">永田　里奈</p>
-          </v-card-item>
-        </v-card>
-      </v-col>
-      <v-col cols="12" md="4">
-        <v-card>
-          <v-card-item>
-            <div class="d-flex justify-center">
-              <v-avatar size="250">
-                <v-img src="https://cdn.vuetifyjs.com/images/john.jpg"></v-img>
-              </v-avatar>
-            </div>
-            <v-divider :thickness="9" color="white"></v-divider>
-            <p class="text-h5 d-flex justify-center">永田　里奈</p>
-          </v-card-item>
-        </v-card>
-      </v-col>
-      <v-col cols="12" md="4">
-        <v-card>
-          <v-card-item>
-            <div class="d-flex justify-center">
-              <v-avatar size="250">
-                <v-img src="https://cdn.vuetifyjs.com/images/john.jpg"></v-img>
-              </v-avatar>
-            </div>
-            <v-divider :thickness="9" color="white"></v-divider>
-            <p class="text-h5 d-flex justify-center">永田　里奈</p>
+            <p class="text-h5 d-flex justify-center">{{ getAuthorName(blog.author_id) }}</p>
           </v-card-item>
         </v-card>
       </v-col>
