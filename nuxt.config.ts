53a55902d60b266c30869f25429843db2412cc36
lina <mad.20th@gmail.com>
2024-06-16 18:35:41 +0900

    add: jwt、sidebase/nuxt-auth導入

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,24 @@
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
-
 export default defineNuxtConfig({
-  ssr: false,
-  devtools: { enabled: true },
+  //...
   build: {
     transpile: ['vuetify'],
   },
   modules: [
+    '@sidebase/nuxt-auth',  // Correct placement of the module
     (_options, nuxt) => {
       nuxt.hooks.hook('vite:extendConfig', (config) => {
         // @ts-expect-error
         config.plugins.push(vuetify({ autoImport: true }))
       })
     },
-    //...
   ],
+  auth: {
+    globalAppMiddleware: true,
+    provider: {
+      type: 'local',
+    },
+  },
   vite: {
     vue: {
       template: {
