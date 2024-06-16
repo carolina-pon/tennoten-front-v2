4244d97c5b15b3828e27c17034af0146a98992e4
lina <mad.20th@gmail.com>
2024-06-16 17:47:16 +0900

    add: POSTリクエスト飛ばせるようにした

--- pages/login.vue
+++ pages/login.vue
@@ -1,4 +1,29 @@
-<script setup></script>
+<script setup>
+// const { signIn } = useAuth();
+const email = ref("");
+const password = ref("");
+const visible = ref(false);
+
+const login = async () => {
+  const response = await useFetch("http://localhost:3000/api/login", {
+    method: 'POST',
+    body:{
+      user:{
+      email: email.value,
+      password: password.value,
+      }
+    },
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  if (response.error.value) {
+    console.error('Login failed:', response.error.value);
+  } else {
+    signIn(response.data);
+  }
+};
+</script>
 
 <template>
   <div class="bg-grey-lighten-3 pt-10 pb-10">
@@ -6,26 +31,27 @@
     <v-container>
       <div>
         <v-card class="mx-auto pa-12 pb-8" max-width="448" rounded="lg" flat>
+          <form @submit.prevent="login">
+            <v-text-field
+              v-model="email"
+              label="Email address"
+              prepend-inner-icon="mdi-email-outline"
+              outlined
+            ></v-text-field>
 
-          <v-text-field
-            density="compact"
-            placeholder="Email address"
-            prepend-inner-icon="mdi-email-outline"
-            variant="outlined"
-          ></v-text-field>
-
-          <v-text-field
-            :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-            :type="visible ? 'text' : 'password'"
-            density="compact"
-            placeholder="Password"
-            prepend-inner-icon="mdi-lock-outline"
-            variant="outlined"
-            @click:append-inner="visible = !visible"
-          ></v-text-field>
-          <v-btn class="mb-8" size="large" variant="tonal" block>
-            LOG IN
-          </v-btn>
+            <v-text-field
+              v-model="password"
+              :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+              :type="visible ? 'text' : 'password'"
+              label="Password"
+              prepend-inner-icon="mdi-lock-outline"
+              outlined
+              @click:append-inner="visible = !visible"
+            ></v-text-field>
+            <v-btn class="mb-8" size="large" color="primary" block type="submit">
+              LOG IN
+            </v-btn>
+          </form>
         </v-card>
       </div>
     </v-container>
